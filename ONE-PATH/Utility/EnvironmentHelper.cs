31f79c1004191611b60a8d010a5024ba0805d1aa
HanRui <54886036@qq.com>
2018-09-21 15:16:39 +0800

    修改名称

---
+++ ONE-PATH/Utility/EnvironmentHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ONE_PATH.Utility.Model;
+
+namespace ONE_PATH.Utility
+{
+    class EnvironmentHelper
+    {
+        public static EnvironmentModel GetEnvironmentPath()
+        {
+            EnvironmentModel model = new EnvironmentModel();
+            JsonObj result = WebAPI.Get("http://api.ieasn.com/?service=App.GetPath.Cmder");
+            if (result.Get("ret") == "200")
+            {
+                Dictionary<string, object> list = result.GetList();
+                Dictionary<string, object> Data = (Dictionary<string, object>)list["data"];
+                model.SystemPath = Data["SystemPath"].ToString();
+                model.UserPath = Data["UserPath"].ToString();
+                model.OtherTitle = Data["OtherTitle"].ToString();
+                model.OtherValue = Data["OtherValue"].ToString();
+            }
+            return model;
+        }
+    }
+}
