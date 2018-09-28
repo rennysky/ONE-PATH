ead4faddc3933edacddacd2f3304d24b0e802cc8
HanRui <54886036@qq.com>
2018-09-28 10:01:49 +0800

    为啥推送不出去呢

--- ONE-PATH/Utility/environmenthelper.cs
+++ ONE-PATH/Utility/environmenthelper.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,21 +9,42 @@ namespace ONE_PATH.Utility
 {
     class EnvironmentHelper
     {
-        public static EnvironmentModel GetEnvironmentPath()
+        public static EnvironmentModel GetEnvironmentPath(string SoftName)
         {
             EnvironmentModel model = new EnvironmentModel();
-            JsonObj result = WebAPI.Get("http://api.ieasn.com/?service=App.GetPath.Cmder");
+            JsonObj result = WebAPI.Get("http://api.ieasn.com/?service=App.GetPath."+SoftName);
             if (result.Get("ret") == "200")
             {
                 Dictionary<string, object> list = result.GetList();
-                Dictionary<string, object> Data = (Dictionary<string, object>)list["data"];
+                Dictionary<string, object> Data = (Dictionary<string, object>) list["data"];
                 model.SystemPath = Data["SystemPath"].ToString();
                 model.UserPath = Data["UserPath"].ToString();
                 model.OtherKey = Data["OtherKey"].ToString();
                 model.OtherValue = Data["OtherValue"].ToString();
                 model.IsMachine = Data["IsMachine"].ToString();
             }
+
             return model;
+        }
+
+        public static ArrayList SoftList = new ArrayList();
+
+        public static void GetEnvironmentPathList()
+        {
+            JsonObj result = WebAPI.Get("http://api.ieasn.com/?service=App.GetSoftEnv.GetPathList");
+            if (result.Get("ret") == "200")
+            {
+                Dictionary<string, object> dic = result.GetList();
+                Dictionary<string, object> data = dic["data"] as Dictionary<string, object>;
+
+                if (data.Count > 0)
+                {
+                    foreach (var KeyList in data.Keys)
+                    {
+                        SoftList.Add(KeyList);
+                    }
+                }
+            }
         }
     }
 }
