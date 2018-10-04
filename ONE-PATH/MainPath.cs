6e04bf3c8678ed2ec2685dc00b08661173b3dc05
renny <xhnr@vip.qq.com>
2018-10-04 21:25:43 +0800

    多余

--- ONE-PATH/MainPath.cs
+++ ONE-PATH/MainPath.cs
@@ -180,7 +180,7 @@ namespace ONE_PATH
         private void timer1_Tick(object sender, EventArgs e)
         {
             timer1.Enabled = false;
-            CheckVersion();
+           // CheckVersion();
         }
 
 
